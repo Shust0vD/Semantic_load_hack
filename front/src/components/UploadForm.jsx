6f5d6f0ea599734ce5a903542537430e76f038bd
Дмитрий Шустов <dshustov@web-bee.ru>
2022-10-11 22:58:47 +0300

    fix: resolve conflicts

--- front/src/components/UploadForm.jsx
+++ front/src/components/UploadForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { uploadFiles } from "../API";
 import Accordion from "react-bootstrap/Accordion";
+import "./UploadFormStyles.css";
 
 const UploadForm = () => {
   const [firstSelectedFile, setFirstSelectedFile] = useState(null);
@@ -41,56 +42,61 @@ const UploadForm = () => {
       const color = part.added ? "green" : part.removed ? "red" : "grey";
       span = document.createElement("span");
       span.style.color = color;
-      span.appendChild(
-        document.createTextNode(
-          part.added || part.removed ? part.value + "[4]" : part.value // Поменять на реальное значение
-        )
-      );
+      span.appendChild(document.createTextNode(part.value)); // Поменять на реальное значение
       fragment.appendChild(span);
     });
     display.appendChild(fragment);
   };
 
   return (
-    <>
-      <div>
-        <h3>Первый файл</h3>
-        <input
-          type="file"
-          accept=".txt" //.doc, .docx, .pdf пока что не поддерживаются
-          onChange={(e) => setFirstSelectedFile(e.target.files[0])}
-        />
-        <h3>Второй файл</h3>
-        <input
-          type="file"
-          accept=".txt" //.doc, .docx, .pdf пока что не поддерживаются
-          onChange={(e) => setSecondSelectedFile(e.target.files[0])}
-        />
-        <button
-          onClick={() => onFileUpload()}
-          disabled={!firstSelectedFile || !secondSelectedFile}
-        >
-          Оценить разницу
-        </button>
+    <div className="page">
+      <div className="page__upload upload">
+        <div className="uploa__field upload__field_first">
+          <h3 className="upload__title upload__title_first">Первый файл</h3>
+          <input
+            type="file"
+            accept=".txt" //.doc, .docx, .pdf пока что не поддерживаются
+            onChange={(e) => setFirstSelectedFile(e.target.files[0])}
+            className="upload__input upload__input_first"
+          />
+        </div>
+        <div className="uploa__field upload__field_second">
+          <h3 className="upload__title upload__title_second">Второй файл</h3>
+          <input
+            type="file"
+            accept=".txt" //.doc, .docx, .pdf пока что не поддерживаются
+            onChange={(e) => setSecondSelectedFile(e.target.files[0])}
+            className="upload__input upload__input_second"
+          />
+        </div>
+        <div className="upload__btn-container">
+          <button
+            onClick={() => onFileUpload()}
+            disabled={!firstSelectedFile || !secondSelectedFile}
+            className="upload__btn"
+          >
+            Оценить разницу
+          </button>
+        </div>
       </div>
 
-      <Accordion>
+      <Accordion className="accordion" alwaysOpen>
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Difference</Accordion.Header>
+          <Accordion.Header>Разница</Accordion.Header>
           <Accordion.Body>
             <span id="display"></span>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>File contents #1</Accordion.Header>
+          <Accordion.Header>Содержание файла #1</Accordion.Header>
           <Accordion.Body>{filesContent.file1}</Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
-          <Accordion.Header>File contents #2</Accordion.Header>
+          <Accordion.Header>Содержание файла #2</Accordion.Header>
           <Accordion.Body>{filesContent.file2}</Accordion.Body>
         </Accordion.Item>
       </Accordion>
-    </>
+    </div>
   );
 };
 
